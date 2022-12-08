e5784d49ec585ff824232a10e5ada647f5d4f25f
ankh04 <ankh04@icloud.com>
2022-12-08 18:07:40 +0800

    指令抽象完成

---
+++ instructions/base/bytecode_reader.go
@@ -0,0 +1,36 @@
+package base
+
+type BytecodeReader struct {
+	code []byte
+	pc   int
+}
+
+func (br *BytecodeReader) Reset(code []byte, pc int) {
+	br.code = code
+	br.pc = pc
+}
+
+func (br *BytecodeReader) ReadUint8() uint8 {
+	res := br.code[br.pc] // 读取八位数据
+	br.pc++
+	return res
+}
+
+func (br *BytecodeReader) ReadUint16() uint16 {
+	byte1 := uint16(br.code[br.pc])   // 读取八位数据
+	byte2 := uint16(br.code[br.pc+1]) // 继续读取八位数据
+	br.pc += 2
+	return (byte1 << 8) | byte2
+}
+
+func (br *BytecodeReader) ReadInt16() int16 {
+	return int16(br.ReadUint16())
+}
+
+func (br *BytecodeReader) ReadInt32() int32 {
+	byte1 := int32(br.ReadUint8())
+	byte2 := int32(br.ReadUint8())
+	byte3 := int32(br.ReadUint8())
+	byte4 := int32(br.ReadUint8())
+	return (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4
+}
---
+++ instructions/instruction.go
@@ -0,0 +1,48 @@
+package instructions
+
+import "MyJVM/rtda"
+import "MyJVM/instructions/base"
+
+type Instruction interface {
+	FetchOperands(reader *base.BytecodeReader)
+	Execute(frame *rtda.Frame)
+}
+
+// 下面创建一些"基质"结构体，方便有类似行为的结构继承
+
+// NoOperandsInstruction 没有操作数的指令结构(方便其他没有操作数的指令继承)
+type NoOperandsInstruction struct{}
+
+func (noi *NoOperandsInstruction) FetchOperands(reader *base.BytecodeReader) {
+	// 没有操作数  什么也不做
+}
+
+// BranchInstruction 跳转指令结构
+type BranchInstruction struct {
+	Offset int
+}
+
+func (bi *BranchInstruction) FetchOperands(reader *base.BytecodeReader) {
+	// 分支指令结构的操作数就是偏移地址
+	bi.Offset = int(reader.ReadInt16())
+}
+
+// Index8Instruction 索引8位数据的指令结构（在加载和存储"局部变量表"的时候会用到这种结构）
+// 会从操作数中读取一个 int8 的整数索引
+type Index8Instruction struct {
+	Index uint
+}
+
+func (i8i *Index8Instruction) FetchOperands(reader *base.BytecodeReader) {
+	i8i.Index = uint(reader.ReadUint8())
+}
+
+// Index16Instruction 索引16位数据的指令结构（在加载和存储"运行时常量池"的时候会用到这种结构）
+// 会从操作数中读取一个 int16 的整数索引
+type Index16Instruction struct {
+	Index uint
+}
+
+func (i16i *Index16Instruction) FetchOperands(reader *base.BytecodeReader) {
+	i16i.Index = uint(reader.ReadUint16())
+}
