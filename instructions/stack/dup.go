97a2e6a0ad4e44bef551774c49ee5ed969d924a8
ankh04 <ankh04@icloud.com>
2022-12-09 22:37:56 +0800

    stack补全

--- instructions/stack/dup.go
+++ instructions/stack/dup.go
@@ -19,3 +19,65 @@ func (dup *DUP) Execute(frame *rtda.Frame) {
 	top := stack.Top()
 	stack.PushSlot(top)
 }
+
+// Execute 赋值一个slot到第二个slot下
+func (dup *DUP_X1) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopSlot()
+	v1 := stack.PopSlot()
+	stack.PushSlot(v2)
+	stack.PushSlot(v1)
+	stack.PushSlot(v2)
+}
+
+// Execute 赋值一个slot到第三个slot下
+func (dup *DUP_X2) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v3 := stack.PopSlot()
+	v2 := stack.PopSlot()
+	v1 := stack.PopSlot()
+	stack.PushSlot(v3)
+	stack.PushSlot(v1)
+	stack.PushSlot(v2)
+	stack.PushSlot(v3)
+}
+
+func (dup *DUP2) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopSlot()
+	v1 := stack.PopSlot()
+	stack.PushSlot(v1)
+	stack.PushSlot(v2)
+	stack.PushSlot(v1)
+	stack.PushSlot(v2)
+}
+
+// Execute 复制头两个slot到第3个slot下
+func (dup *DUP2_X1) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v3 := stack.PopSlot()
+	v2 := stack.PopSlot()
+	v1 := stack.PopSlot()
+
+	stack.PushSlot(v2)
+	stack.PushSlot(v3)
+	stack.PushSlot(v1)
+	stack.PushSlot(v2)
+	stack.PushSlot(v3)
+}
+
+// Execute 复制头两个slot到第4个slot下
+func (dup *DUP2_X2) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v4 := stack.PopSlot()
+	v3 := stack.PopSlot()
+	v2 := stack.PopSlot()
+	v1 := stack.PopSlot()
+
+	stack.PushSlot(v3)
+	stack.PushSlot(v4)
+	stack.PushSlot(v1)
+	stack.PushSlot(v2)
+	stack.PushSlot(v3)
+	stack.PushSlot(v4)
+}
